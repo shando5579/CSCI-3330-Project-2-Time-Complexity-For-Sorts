7e17b47f5623a3408100b2ce6f28f55ba7bca241
shandonp5579 <sprobst@cub.uca.edu>
2022-03-04 01:58:26 -0600

    Added globals

--- data_generation.py
+++ data_generation.py
@@ -11,9 +11,21 @@ various sorting algorithms.
 
 import random
 
+# Global values for use in other files
+global neutraldata10_perm
+global neutraldata100_perm
+global neutraldata1000_perm
+global ordereddata10_perm
+global ordereddata100_perm
+global ordereddata1000_perm
+global reversedata10_perm
+global reversedata100_perm
+global reversedata1000_perm
+
 def defaultData():
     'Used for creating array data'
 
+    # Random number range
     ranRange = range(1, 1000)
 
     # Neutral data
@@ -36,7 +48,29 @@ def defaultData():
     reversedata1000 = list(range(1, 1001))
     reversedata1000.reverse()
 
-    # Chunk that prints arrays
+    # Global variables defined
+    global neutraldata10_perm
+    neutraldata10_perm = neutraldata10
+    global neutraldata100_perm
+    neutraldata100_perm = neutraldata100
+    global neutraldata1000_perm
+    neutraldata1000_perm = neutraldata1000
+
+    global ordereddata10_perm
+    ordereddata10_perm = ordereddata10
+    global ordereddata100_perm
+    ordereddata100_perm = ordereddata100
+    global ordereddata1000_perm
+    ordereddata1000_perm = ordereddata1000
+
+    global reversedata10_perm
+    reversedata10_perm = reversedata10
+    global reversedata100_perm
+    reversedata100_perm = reversedata100
+    global reversedata1000_perm
+    reversedata1000_perm = reversedata1000
+
+'''    # Chunk that prints arrays
     print("Neutral DATA (10): ", neutraldata10)
     print("\nNeutral DATA (100): ", neutraldata100)
     print("\nNeutral DATA (1000): ", neutraldata1000)
@@ -51,4 +85,4 @@ def defaultData():
 
     
 defaultData()
-
+'''
