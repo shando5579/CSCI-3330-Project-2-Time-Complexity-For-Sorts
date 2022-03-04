2fa5885c1db3dce5ea0bc13ab31cbd11a3ff718d
shandonp5579 <sprobst@cub.uca.edu>
2022-03-04 01:05:56 -0600

    Start of front end

---
+++ front_end.py
@@ -0,0 +1,14 @@
+"""
+@author: Shandon Probst
+@project: Algorithms Project 1
+@file: front_end.py
+
+This file is used as the main driver and shows the
+measurements of time for algorithms being tested with
+randomly generated arrays with sizes in the multiples 
+of 10's (array sizes 10, 100, 1000)
+
+"""
+
+import data_generation
+import sorts
