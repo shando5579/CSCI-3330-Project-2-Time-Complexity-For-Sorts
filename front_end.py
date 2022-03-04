3bb39ba68f2e1cfebf83eae533c0633d45e438dd
shandonp5579 <sprobst@cub.uca.edu>
2022-03-04 01:58:15 -0600

    Progress on front end

--- front_end.py
+++ front_end.py
@@ -12,3 +12,104 @@ of 10's (array sizes 10, 100, 1000)
 
 import data_generation
 import sorts
+import time
+
+def main():
+    '''Main driver'''
+
+    # Temporary arrays
+    test_array1 = []
+    test_array2 = []
+    test_array3 = []
+
+    # Timers
+    time1 = 0
+    time1_t = 0
+    time1_f = 0
+
+    time2 = 0
+    time2_t = 0
+    time2_f = 0
+
+    time3 = 0
+    time3_t = 0
+    time3_f = 0
+
+    # Initializes test arrays
+    print('Initializing test arrays.\n')
+    data_generation.defaultData()
+
+    # Bubble sort testing
+    print('1. Testing bubble sort.')
+
+    # Sorting with arrays [10]
+    print('\nSorting arrays with size = 10')
+    
+    print('\tSorting neutral data...')
+    time1 = time.time()
+    test_array1 = sorts.bubble_sort(data_generation.neutraldata10_perm)
+    time1_t = time.time()
+    time1_f = time1_t - time1
+    print('\t\tTime taken: ', time1_f)
+    
+    print('\tSorting ordered data...')
+    time2 = time.time()
+    test_array2 = sorts.bubble_sort(data_generation.ordereddata10_perm)
+    time2_t = time.time()
+    time2_f = time2_t - time2
+    print('\t\tTime taken: ', time2_f)
+
+    print('\tSorting reversed data...')
+    time3 = time.time()
+    test_array3 = sorts.bubble_sort(data_generation.reversedata10_perm)
+    time3_t = time.time()
+    time3_f = time3_t - time3
+    print('\t\tTime taken: ', time3_f)
+
+    # Sorting with arrays [100]
+    print('\nSorting arrays with size = 100')
+    print('\tSorting neutral data...')
+    time1 = time.time()
+    test_array1 = sorts.bubble_sort(data_generation.neutraldata100_perm)
+    time1_t = time.time()
+    time1_f = time1_t - time1
+    print('\t\tTime taken: ', time1_f)
+    
+    time2 = time.time()
+    print('\tSorting ordered data...')
+    test_array2 = sorts.bubble_sort(data_generation.ordereddata100_perm)
+    time2_t = time.time()
+    time2_f = time2_t - time2
+    print('\t\tTime taken: ', time2_f)
+
+    time3 = time.time()
+    print('\tSorting reversed data...')
+    test_array3 = sorts.bubble_sort(data_generation.reversedata100_perm)
+    time3_t = time.time()
+    time3_f = time3_t - time3
+    print('\t\tTime taken: ', time3_f)
+
+    # Sorting with arrays [1000]
+    print('\nSorting arrays with size = 1000')
+    print('\tSorting neutral data...')
+    time1 = time.time()
+    test_array1 = sorts.bubble_sort(data_generation.neutraldata1000_perm)
+    time1_t = time.time()
+    time1_f = time1_t - time1
+    print('\t\tTime taken: ', time1_f)
+    
+    time2 = time.time()
+    print('\tSorting ordered data...')
+    test_array2 = sorts.bubble_sort(data_generation.ordereddata1000_perm)
+    time2_t = time.time()
+    time2_f = time2_t - time2
+    print('\t\tTime taken: ', time2_f)
+
+    time3 = time.time()
+    print('\tSorting reversed data...')
+    test_array3 = sorts.bubble_sort(data_generation.reversedata1000_perm)
+    time3_t = time.time()
+    time3_f = time3_t - time3
+    print('\t\tTime taken: ', time3_f)
+
+main()
