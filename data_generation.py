77df4a31e539991b8db7c43bd4e367dd6fedfccc
shandonp5579 <sprobst@cub.uca.edu>
2022-03-13 12:54:00 -0500

    Fixed ordered and reversed list generation.

--- data_generation.py
+++ data_generation.py
@@ -8,81 +8,81 @@ with random integer values to later be sorted with
 various sorting algorithms.
 
 """
-
 import random
 
 # Global values for use in other files
-global neutraldata10_perm
-global neutraldata100_perm
-global neutraldata1000_perm
-global ordereddata10_perm
-global ordereddata100_perm
-global ordereddata1000_perm
-global reversedata10_perm
-global reversedata100_perm
-global reversedata1000_perm
+global neutraldata35_perm
+global neutraldata350_perm
+global neutraldata3500_perm
+global ordereddata35_perm
+global ordereddata350_perm
+global ordereddata3500_perm
+global reverseddata35_perm
+global reverseddata350_perm
+global reverseddata3500_perm
 
 def defaultData():
     'Used for creating array data'
 
     # Random number range
-    ranRange = range(1, 1000)
+    ranRange = range(1, 10000)
 
     # Neutral data
-    neutraldata10 = random.choices(ranRange, k = 11)
-    neutraldata100 = random.choices(ranRange, k = 101)
-    neutraldata1000 = random.choices(ranRange, k = 1001)
+    neutraldata35 = random.choices(ranRange, k = 35)
+    neutraldata350 = random.choices(ranRange, k = 350)
+    neutraldata3500 = random.choices(ranRange, k = 3500)
 
     # Ordered data
-    ordereddata10 = list(range(1, 11))
-    ordereddata100 = list(range(1, 101))
-    ordereddata1000 = list(range(1, 1001))
+    ordereddata35 = neutraldata35.copy()
+    ordereddata35.sort()
+    ordereddata350 = neutraldata350.copy()
+    ordereddata350.sort()
+    ordereddata3500 = neutraldata3500.copy()
+    ordereddata3500.sort()
 
     # Reversed data
-    reversedata10 = list(range(1, 11))
-    reversedata10.reverse()
-
-    reversedata100 = list(range(1, 101))
-    reversedata100.reverse()
-
-    reversedata1000 = list(range(1, 1001))
-    reversedata1000.reverse()
+    reverseddata35 = ordereddata35.copy()
+    reverseddata35.reverse()
+    reverseddata350 = ordereddata350.copy()
+    reverseddata350.reverse()
+    reverseddata3500 = ordereddata3500.copy()
+    reverseddata3500.reverse()
 
     # Global variables defined
-    global neutraldata10_perm
-    neutraldata10_perm = neutraldata10
-    global neutraldata100_perm
-    neutraldata100_perm = neutraldata100
-    global neutraldata1000_perm
-    neutraldata1000_perm = neutraldata1000
+    global neutraldata35_perm
+    neutraldata35_perm = neutraldata35
+    global neutraldata350_perm
+    neutraldata350_perm = neutraldata350
+    global neutraldata3500_perm
+    neutraldata3500_perm = neutraldata3500
 
-    global ordereddata10_perm
-    ordereddata10_perm = ordereddata10
-    global ordereddata100_perm
-    ordereddata100_perm = ordereddata100
-    global ordereddata1000_perm
-    ordereddata1000_perm = ordereddata1000
+    global ordereddata35_perm
+    ordereddata35_perm = ordereddata35
+    global ordereddata350_perm
+    ordereddata350_perm = ordereddata350
+    global ordereddata3500_perm
+    ordereddata3500_perm = ordereddata3500
 
-    global reversedata10_perm
-    reversedata10_perm = reversedata10
-    global reversedata100_perm
-    reversedata100_perm = reversedata100
-    global reversedata1000_perm
-    reversedata1000_perm = reversedata1000
+    global reverseddata35_perm
+    reverseddata35_perm = reverseddata35
+    global reverseddata350_perm
+    reverseddata350_perm = reverseddata350
+    global reverseddata3500_perm
+    reverseddata3500_perm = reverseddata3500
 
-'''    # Chunk that prints arrays
-    print("Neutral DATA (10): ", neutraldata10)
-    print("\nNeutral DATA (100): ", neutraldata100)
-    print("\nNeutral DATA (1000): ", neutraldata1000)
+'''
+    # Chunk that prints arrays
+    print("Neutral DATA (35): ", neutraldata35)
+    print("\nNeutral DATA (350): ", neutraldata350)
+    print("\nNeutral DATA (3500): ", neutraldata3500)
 
-    print("\nORDERED DATA (10): ", ordereddata10)
-    print("\nORDERED DATA (100): ", ordereddata100)
-    print("\nORDERED DATA (1000): ", ordereddata1000)
+    print("\nORDERED DATA (35): ", ordereddata35)
+    print("\nORDERED DATA (350): ", ordereddata350)
+    print("\nORDERED DATA (3500): ", ordereddata3500)
 
-    print("\nREVERSED DATA (10): ", reversedata10)
-    print("\nREVERSED DATA (100): ", reversedata100)
-    print("\nREVERSED DATA (1000): ", reversedata1000)
-
+    print("\nREVERSED DATA (35): ", reverseddata35)
+    print("\nREVERSED DATA (350): ", reverseddata350)
+    print("\nREVERSED DATA (3500): ", reverseddata3500)
     
 defaultData()
 '''
